4a6237a0e81c229a04f348fe912a333ad3ca778e
Emil Pradeep <emilpradeep@gmail.com>
2020-12-01 10:19:35 +0530

    implmented spring saml sso implementation using okta IDO

---
+++ src/test/java/com/tech/springsaml/SpringSamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.springsaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
